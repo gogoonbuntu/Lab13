a8c85a579465748a1742fbaabe059505d9e7a737
GGBT <tmddud333@naver.com>
2018-04-30 15:31:54 +0900

    prob5 comments ok

--- src/main/java/edu/handong/csee/java/lab13/prob5/Equal.java
+++ src/main/java/edu/handong/csee/java/lab13/prob5/Equal.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Equal {
 	public static void main(String[] args)
 	{
-		int r1, c1, r2, c2;
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter row size(maximum 5): ");
+		int r1, c1, r2, c2; //create two row sizes, two column sizes.
+		Scanner sc = new Scanner(System.in); //a new scanner
+		System.out.print("Enter row size(maximum 5): "); //get input from keyboards for 4 integers
 		r1=sc.nextInt();
 		System.out.print("Enter column size(maximum 5): ");
 		c1=sc.nextInt();
@@ -15,27 +15,26 @@ public class Equal {
 		r2=sc.nextInt();
 		System.out.print("Enter column size(maximum 5): ");
 		c2=sc.nextInt();
-		System.out.print("Enter List1: ");
-		int[][] ar1 = makeArray(r1, c1);
-		System.out.print("Enter List2: ");
-		int[][] ar2 = makeArray(r2, c2);
-		printArray(ar1);
+		System.out.print("Enter List1: "); //get input for array.
+		int[][] ar1 = makeArray(r1, c1); //reference makeArray functions
+		System.out.print("Enter List2: "); //get input for array.
+		int[][] ar2 = makeArray(r2, c2);//reference makeArray functions
+		printArray(ar1);//reference printArray functions
 		printArray(ar2);
-		equal(ar1, ar2);
+		equal(ar1, ar2);//equal function to compare two arrays.
 	}
 	public static int[][] makeArray(int r1, int c1)
 	{
-		int[][] ar1 = new int[r1][c1];
-		Scanner sc1 = new Scanner(System.in);
+		int[][] ar1 = new int[r1][c1]; // create a array in local memory
+		Scanner sc1 = new Scanner(System.in); // a new scanner
 		for(int i=0;i<r1;i++)
 		{
 			for(int j=0;j<c1;j++)
 			{
-				ar1[i][j]=sc1.nextInt();
+				ar1[i][j]=sc1.nextInt(); //get inputs from keyboard for each element
 			}
-			System.out.print("\n");
 		}
-		return ar1;
+		return ar1; //return the array made.
 	}
 	public static void printArray(int[][] ar)
 	{
@@ -43,19 +42,19 @@ public class Equal {
 		{
 			for(int j=0;j<ar[i].length;j++)
 			{
-				System.out.print(ar[i][j]+" ");
+				System.out.print(ar[i][j]+" "); //print arrays and put a space between elements.
 			}
-			System.out.print("\n");
+			System.out.print("\n"); //change line for view.
 		}
 		System.out.print("\n");
 	}
 	public static void equal(int[][] ar1, int[][] ar2)
 	{
-		int ercount=0;
-		boolean k=true;
+		int ercount=0; // stands for error count
+		boolean k=true; // boolean for final decision. true for equal, false for NOT equal.
 		if(ar1.length!=ar2.length)
 		{
-			k=false;
+			k=false; // if length(row) is not equal, make the result UNequal.
 		}
 		else
 		{
@@ -65,11 +64,11 @@ public class Equal {
 				{
 					if(ar1[i][j]!=ar2[i][j])
 					{
-						ercount++;
+						ercount++; // if any element is not equal, error count increase.
 					}
 				}
 			}
-			if(ercount>3) {k=false;}
+			if(ercount>3) {k=false;} // if error count bigger than 3, make the result UNequal.
 		}
 		if(k) {System.out.println("The two arrays are approximately identical.");}
 		else {System.out.println("The two arrays are NOT identical.");}
